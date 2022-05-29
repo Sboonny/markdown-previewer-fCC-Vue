c53e2e7d7c551aea78c539437628cfc68ee46e4b
Sboonny <MuhammedElruby@gmail.com>
2022-05-29 20:40:47 +0200

    added simple markdown data

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,60 @@
-<script setup lang="ts">
+<script lang="ts">
+import { defineComponent } from 'vue';
 
+export default defineComponent({
+  name: "Markdown",
+  data() {
+    return {
+      markdownPlaceholder: `# Welcome to my Vue Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Here's some code, \`< div > </div>\`, between 2 backticks.
+
+          \`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+You can also make text ** bold **...whoa!
+Or _italic_.
+    Or...wait for it... ** _both!_**
+  And feel free to go crazy ~~crossing stuff out~~.
+
+    There's also [links](https://www.freecodecamp.org), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header ?
+  ------------ | ------------- | -------------
+    Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbered lists too.
+1. Use just 1s if you want!
+1. And last but not least, let's not forget embedded images:
+
+![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+  `
+    }
+  },
+  mounted() {
+    this.markdownPlaceholder
+  }
+}) 
 </script>
 
 <template>
@@ -7,7 +62,7 @@
     <form>
       <label for="editor">Markdown-Editor:</label>
 
-      <textarea id="editor" name="markdown-editor" spellcheck="true">
+      <textarea id="editor" name="markdown-editor" spellcheck="true">{{ markdownPlaceholder }}
 </textarea>
       <label for="previewer">Markdown-Previwer:</label>
 
